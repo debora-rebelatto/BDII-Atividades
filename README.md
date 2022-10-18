e037b4cdd2f7e894778b0529568239581150e4ef
debora.rebelatto <deborarebelatto1@gmail.com>
2022-10-18 17:25:44 -0300

    atividades

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # BDII-Atividades
 
 Repositório de atividades da matéria de BDII.
-```sudo -i -u postgres
-``` 
-<!-- extract tar.xz file -->
--- atividade2/answers.sql
+++ atividade2/answers.sql
@@ -1,4 +1,8 @@
--- se o produto em estoque chegar ao valor 0 (não tem mais o produto) então o mesmo deve ser removido das tabelas Product e Stock. A aplicação faz o controle para o caso de quantity<=0. 
--- if product reaches 0, remove from tables Product and Stock
--- if quantity<=0, the application will control it
-DELETE FROM Product WHERE id NOT IN (SELECT product_id FROM Stock);
+CREATE OR REPLACE FUNCTION remove_product()
+RETURNS TRIGGER AS $$
+BEGIN
+    DELETE FROM Product WHERE id = OLD.product_id;
+    DELETE FROM Stock WHERE product_id = OLD.product_id;
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
